879e5bbb41f78751e2029550e1da69d187d83313
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-14 22:15:42 +0100

    Adding Navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
       {/* NAVBAR */}
-
+      <Navbar />
       {/* HOME CARRUSEL */}
+      {/* VIDEO */}
+      
 
       {/* OUR SOFTWARE IS USED DAILY BY */}
 
