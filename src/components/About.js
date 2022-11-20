21c92fcc25e2ab3122b9252af5ea67fe644d0d6c
EcaCosca <enriquecoscarelli@gmail.com>
2022-11-20 22:58:42 +0100

    About content

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,11 @@ const About = () => {
   return (
     <div>
         <h4 className='font-mono leading-tight text-5xl mt-0 mb-4 text-gray-400 border-t-2 p-6'>ABOUT</h4>
+        <div>
+            <p className='font-mono my-2 p-4'>Since 2009, Icon has delivered bespoke software for the sophisticated processing and analysis of technical data alongside a compelling, intuitive user experience.</p>
+            <p className='font-mono my-2 p-4'>We employ rigorous software engineering practices whilst exploiting our efficiency to deliver quickly and to readily adapt to evolving client requirements.</p>
+            <p className='font-mono my-2 p-4'>Icon is based at the University of Nottingham’s Innovation Park with a branch office in the Chamonix valley, France.</p>
+        </div>
     </div>
   )
 }
